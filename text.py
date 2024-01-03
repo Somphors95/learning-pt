04c609008ab1fa16484309be63982f3eb47668d4
Som phors <somphorstae9@gmail.com>
2024-01-03 19:20:11 +0700

    love

---
+++ text.py
@@ -0,0 +1,19 @@
+# x = str(3)
+# y = int(3)
+# z = float(3)
+
+# print(type(x))
+# print(y)
+# print(z)
+
+myvar = "Jonh"
+my_var = "jin"
+_my_var = "Rm"
+MYVAR = "jungkook"
+myvar2 = "jimin"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(MYVAR)
+print(myvar2)
